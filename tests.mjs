6abc241ce2200c6c0db09e360041b890a64ddcb2
CheryX <58445363+CheryX@users.noreply.github.com>
2022-03-26 16:24:14 +0100

    Added rendering minecraft skin

--- tests.mjs
+++ tests.mjs
@@ -1,14 +1,23 @@
 import Canvas from 'canvas'; 
 import textBox from './lib/textBox.mjs';
+import drawSkin from './lib/drawSkin.mjs';
 import fs from 'fs';
 
+// Register Minecraft Font
+Canvas.registerFont('./assets/fonts/Minecraftia.otf', { family: 'Minecraft' })
+Canvas.registerFont('./assets/fonts/MinecraftiaBold.otf', { family: 'Minecraft' })
+
 // Create a new canvas
-const canvas = new Canvas.createCanvas(500, 100);
+const canvas = new Canvas.createCanvas(1920, 1080);
 const ctx = canvas.getContext('2d');
 
 // Create background
 ctx.fillStyle = '#ffffff';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+// Create a rectangle
+ctx.fillStyle = '#000000';
+ctx.strokeRect(550, 50, 500, 100);
 
 // Create a new textbox
 let username = [
@@ -21,12 +30,13 @@ let username = [
         color: '#ff0000',
     },
     {
-        text: 'WORKING!',
+        text: 'AMERICA',
         color: '#0000ff',
     }
 ];
 
-new textBox(0, 0, 500, 100, username, 100, 'Arial').draw(ctx);
+new textBox(550, 50, 500, 100, username, 100, 'Minecraft').draw(ctx);
+ctx.drawImage(new drawSkin(32, 'CheryX'), 500, 500);
 
 // Export the canvas to a file
 const out = fs.createWriteStream('./test.png');
