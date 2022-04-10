2ccc8a6a698f7e69d4595327e48e64aef2f0812d
CheryX <roxmb25@gmail.com>
2022-04-10 23:10:45 +0200

    Tested some practical uses for textBox

--- tests.mjs
+++ tests.mjs
@@ -1,4 +1,5 @@
 // Some sort of unit tests
+// WARNING! DO NOT TEST HYPIXEL API SINCE THERE IS NO API KEY
 
 import Canvas from 'canvas'; 
 import textBox from './lib/textBox.mjs';
@@ -9,7 +10,7 @@ import fs from 'fs';
 
 // Register Minecraft Font
 Canvas.registerFont('./assets/fonts/Minecraftia.otf', { family: 'Minecraft' })
-Canvas.registerFont('./assets/fonts/MinecraftiaBold.otf', { family: 'Minecraft' })
+Canvas.registerFont('./assets/fonts/MinecraftiaBold.otf', { family: 'MinecraftBold' })
 
 // Create a new canvas
 const canvas = new Canvas.createCanvas(1000, 300);
@@ -20,33 +21,63 @@ const template = new Canvas.Image();
 template.src = fs.readFileSync('./assets/images/template2.png');
 ctx.drawImage(template, 0, 0);
 
-// Set up nickname
-let username = 'cheryx';
-let coloredUsername = await getNickname(username);
+// Just to make sure that it won't test the API (advanced tests coming soon)
+if (false) {
 
-// Create a new textbox
-let x = 238
-let y = 95
-let width = 290
-let height = 38
-new textBox(x, y, width, height, coloredUsername, 90, 'Minecraft', ['bottom', 'left']).draw(ctx);
+    // Set up nickname
+    let username = 'electruuu';
+    let coloredUsername = await getNickname(username);
+    
+    // Create a new textbox
+    new textBox(238, 95, 290, 38, coloredUsername, 90, 'Minecraft', ['bottom', 'left']).draw(ctx);
+    
+    // Test drawSkin
+    let mcSkin = await drawSkin(8, username);
+    await ctx.drawImage(mcSkin, 75, 77);
+    
+    let userProfile = await profile(username);
+    console.log(userProfile)
+    
+    // Create XP text
+    ctx.font = '20px MinecraftBold';
+    ctx.fillStyle = '#ffffff';
+    
+    let xpFormatted = [
+        {
+            text: `${Math.floor(userProfile.level)} LVL (${(100*userProfile.level - 100*Math.floor(userProfile.level)).toFixed(2)}% XP)`,
+            color: '#ffffff',
+            shadow: {
+                color: '#555555',
+                x: 10,
+                y: 10,
+                blur: 0
+            }
+        }
+    ]
+    
+    let achievements = [
+        {
+            text: `${userProfile.badges.length} Badges (-40%)`,
+            color: '#ffffff',
+            shadow: {
+                color: '#555555',
+                x: 10,
+                y: 10,
+                blur: 0
+            }
+        }
+    ]
+    
+    new textBox(240, 165, 250, 20, xpFormatted, 20, 'MinecraftBold', ['center', 'left']).draw(ctx);
+    new textBox(240, 188, 250, 20, achievements, 15, 'MinecraftBold', ['center', 'left']).draw(ctx);
+    
+    let scaled = new Canvas.createCanvas(4000, 1200);
+    let scaledCtx = scaled.getContext('2d');
+    scaledCtx.drawImage(canvas, 0, 0, 4000, 1200);
+    
+    // Export the canvas to a file
+    const out = fs.createWriteStream('./test.png');
+    const stream = scaled.createPNGStream();
+    stream.pipe(out);
 
-// Draw outline
-ctx.strokeStyle = '#000000';
-ctx.lineWidth = 2;
-//ctx.strokeRect(x, y, width, height);
-
-// Test drawSkin
-let mcSkin = await drawSkin(8, username);
-await ctx.drawImage(mcSkin, 75, 77);
-
-let userProfile = await profile(username);
-console.log(userProfile)
-
-let xpFormatted = `${Math.floor(userProfile.level)} LVL (${(100*userProfile.level - 100*Math.floor(userProfile.level)).toFixed(2)}% XP)`;
-//console.log(xpFormatted)
-
-// Export the canvas to a file
-const out = fs.createWriteStream('./test.png');
-const stream = canvas.createPNGStream();
-stream.pipe(out);
+}
