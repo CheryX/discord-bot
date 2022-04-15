2cabacca7ee1acc90a473023cdadce8ca0d8f826
CheryX <roxmb25@gmail.com>
2022-04-15 14:20:26 +0200

    Create badge display

--- commands/hystats.mjs
+++ commands/hystats.mjs
@@ -1,6 +1,7 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import generateBadge from '../lib/generator/badge.mjs';
 import getArgs from '../lib/getArgs.mjs';
+import fetch from 'node-fetch';
 
 const data = new SlashCommandBuilder()
 	.setName('stats')
@@ -27,10 +28,49 @@ const data = new SlashCommandBuilder()
 export default {
 	data,
 	async execute(interaction) {
+		
+		// Defer reply
 		await interaction.deferReply();
-
 		const args = getArgs(interaction);
-		const png = await generateBadge(args.username);
+
+		// Check if username is valid
+		let uuid;
+
+		if (args.username.length <= 16) {
+			let mcResponse = await fetch(`https://api.mojang.com/users/profiles/minecraft/${args.username}`);
+
+			// Check if Mojang API is working
+			if (mcResponse.status == 200) {
+				let minecraftData = await mcResponse.json();
+				
+				//If the user doesn't exist 
+				if (minecraftData.error == "BadRequestException") {
+					return await interaction.editReply('This username is invalid');
+				}
+				
+				uuid = minecraftData.id;
+			} else {
+				return await interaction.editReply('This username is invalid');
+			}
+		} else {
+			return await interaction.editReply('This username is invalid');
+		}
+
+		let png;
+		
+		// Subcommand handler
+		switch (interaction.options._subcommand) {
+			case 'badge': {
+				png = await generateBadge(args.username);
+				break;
+			}
+			case 'bedwars': {
+				//soon^tm
+				//const png = await generateBedwars(args.username);
+				break;
+			}
+		}
+		
 
 		await interaction.editReply({
 			files: [{ attachment: png, name: 'stats.png' }]
