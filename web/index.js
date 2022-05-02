26062c03b1bc28031baabfdb003b21f5bddaa056
CheryX <roxmb25@gmail.com>
2022-05-02 19:52:18 +0200

    Removed EJS since i won't use it

--- web/index.js
+++ web/index.js
@@ -6,29 +6,24 @@ import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const PORT = 2137;
+const URL = `http://localhost:${PORT}/`;
 
 async function run() {
 
     const app = express()
-
-    app.set('view engine', 'ejs');
     app.use(express.static(__dirname + '/public'));
 
-    const pages = [];
     const pageFiles = fs.readdirSync('./web/routes').filter(file => file.endsWith('.js'));
-
     for (const file of pageFiles) {
 
-        // Import the page using import
         const command = await import(`./routes/${file}`);
-
-        // Set the command name to the command
         app.use(command.default.path, command.default.router);
+
     }
 
-    //Listen to website
-    app.listen(8000, () => {
-        log.info("Website listening on http://localhost:8000") 
+    app.listen(PORT, () => {
+        log.info(`Website listening on ${URL}`) 
     });
 
 }
